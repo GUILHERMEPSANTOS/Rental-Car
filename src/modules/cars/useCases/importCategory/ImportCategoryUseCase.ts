8754f2106c19855409c9704b0eec6484dd11796c
GUILHERMEPSANTOS <GUILHERME.PSANTOS@bandtec.com.br>
2022-07-02 11:16:36 -0300

    apagando arquivos temporarios com fs

--- src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -30,6 +30,7 @@ class ImportCategoryUseCase {
                     , description
                 });
             }).on('end', () => {
+                fs.promises.unlink(file.path);
                 resolve(categories);
             }).on('error', (err) => {
                 reject(err);
